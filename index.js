8245890ae540289fb29b0e4ff419c1fd69481d19
pierremamay647-glitch <pierremamay647@gmail.com>
2026-09-08 12:55:03 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import connectToWhatsApp from './auth/authHandler.js';
-import handleIncomingMessage from './events/messageHandler.js';
-import { startPairingServer } from './web/server.js';
-
-(async () => {
-    await connectToWhatsApp(handleIncomingMessage);
-    await startPairingServer(handleIncomingMessage);
-})();
